71c6f87ae611ab0064aa679f1295ab16dc755ac2
alipiaibrahim <ibrahimalipia56@gmail.com>
2021-02-26 13:08:28 +0700

    menambahkan dropdown

--- index.php
+++ index.php
@@ -8,7 +8,17 @@
     <link rel="stylesheet" href="./css/bootstrap.min.css">
 </head>
 <body>
-    
+    <!-- dropdown -->
+<div class="dropdown">
+  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown link
+  </a>
+
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem mollitia vitae, obcaecati dolore esse qui quas nemo accusantium sint ex, 
+    doloremque dolorem labore vel, quo tenetur dolorum aliquid. Voluptatibus, est.</p>
+  </div>
+</div>
     <script src="./js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
