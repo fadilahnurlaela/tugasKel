bf053ea36da917b41c7e39019018d8017cc22009
fadilahnurlaela <fadilahnurlaila366@gmail.com>
2021-02-26 00:47:53 -0800

    menambahkan style

--- index.php
+++ index.php
@@ -10,7 +10,99 @@
 </head>
 
 <body>
-    cekcek
+    <table>
+        <div class="card text-center">
+            <div class="card-body">
+                <h5 class="card-title"> About Drink</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <tr class="tr">
+                    <td>
+                        <div class="container">
+                            <div class="card" style="width: 18rem;">
+                                <img src="../tugasKel/img/drk 1.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-text">Ice Blended</p>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="container">
+                            <div class="card" style="width: 18rem;">
+                                <img src="../tugasKel/img/drk 6.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-text">coffe hot</p>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="container">
+                            <div class="card" style="width: 18rem;">
+                                <img src="../tugasKel/img/drk 7.jpeg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-text">Ice green tea</p>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="container">
+                            <div class="card" style="width: 18rem;">
+                                <img src="../tugasKel/img/drk 2.jpeg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-text">Ice green tea</p>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="container">
+                            <div class="card" style="width: 18rem;">
+                                <img src="../tugasKel/img/drk 4.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-text">Drink tradisional of betawi</p>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="container">
+                            <div class="card" style="width: 18rem;">
+                                <img src="../tugasKel/img/drk 3.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-text">Ice green tea</p>
+
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="container">
+                            <div class="card" style="width: 18rem;">
+                                <img src="../tugasKel/img/drk 7.jpeg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-text">Ice green tea</p>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="container">
+                            <div class="card" style="width: 18rem;">
+                                <img src="../tugasKel/img/drk 7.jpeg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <p class="card-text">Ice green tea</p>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </div>
+        </div>
+    </table>
     <script src="./js/bootstrap.bundle.min.js"></script>
 </body>
 
