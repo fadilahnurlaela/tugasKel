3ef8ab00058b6141fd6dca670669d8882226f4b4
alipiaibrahim <ibrahimalipia56@gmail.com>
2021-03-02 22:32:01 +0700

    menambahkan footer

--- index.php
+++ index.php
@@ -11,22 +11,73 @@
 <body>
     <!-- About -->
     <div class="container-fluid">
-    <div class="article">
-    <h1 class="ah1">About</h1>
-    <p class="p1">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque quam cum distinctio, sunt dolor vitae accusamus, 
-        nihil est cupiditate aliquid molestiae porro quis soluta perspiciatis, nisi enim voluptatibus velit officia.</p>
-    <div class="btn-group">
-        <button type="button" class="btn" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
-            <img src="img/panah.png" class="panah">
-            Left-aligned but right aligned when large screen
-        </button>
-        <p class="dropdown-menu dropdown-menu-lg-end">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Adipisci velit sequi iste, dolorem labore distinctio quibusdam 
-            enim harum explicabo veniam, error amet aspernatur debitis libero. 
-            Cupiditate est ex eum! Magnam.</p>
+        <div class="article">
+            <h1 class="ah1">About</h1>
+                <p class="p1">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque quam cum distinctio, sunt dolor vitae accusamus, 
+                nihil est cupiditate aliquid molestiae porro quis soluta perspiciatis, nisi enim voluptatibus velit officia.</p>
+            <div class="btn-group">
+                <button type="button" class="btn" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+                    <img src="img/panah.png" class="panah">
+                    Left-aligned but right aligned when large screen
+                </button>
+                <p class="dropdown-menu dropdown-menu-lg-end">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Adipisci velit sequi iste, dolorem labore distinctio quibusdam 
+                    enim harum explicabo veniam, error amet aspernatur debitis libero. 
+                    Cupiditate est ex eum! Magnam.</p>
+            </div>
+        </div>
     </div>
+    <!-- Footer -->
+<footer class="footer-section spad">
+
+<div class="footer-widget">
+    <div class="row">
+        <div class="col-lg-3 col-sm-6">
+            <div class="single-footer-widget">
+                <table width="100%" class="table">
+                    <tr>
+                        <td>
+                            <h4>Produk</h4>
+                            <ul>
+                                <li>Pinjaman Tunai</li>
+                                <li>Akucicil</li>
+                                <li>Layanan Kami</li>
+                            </ul>
+                        </td>
+                        <td>
+                            <h4>Artikel</h4>
+                            <ul>
+                                <li>Seputar Keuangan</li>
+                                <li>Berita</li>
+                                <li>Blog</li>
+                            </ul>
+                        </td>
+                        <td>
+                            <h4>Akulaku</h4>
+                            <ul>
+                                <li>Tentang</li>
+                                <li>Bergabunglah</li>
+                                <li>Partner Kami</li>
+                            </ul>
+                        </td>
+                        <td>
+                            <h4>Bantuan</h4>
+                            <ul>
+                                <li>PAQ</li>
+                                <li>Hubungi</li>
+                                <li>Privasi</li>
+                                <li>Persyaratan</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </div>
-    </div>
+</div>
+</footer>
     <script src="./js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
