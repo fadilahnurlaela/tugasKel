d71ef63e48974e8dee02a53881713150409d8eac
fadilahnurlaela <fadilahnurlaila366@gmail.com>
2021-03-12 00:34:35 -0800

    menambahkan deskripsi dalam menu drink dan dessert

--- index.php
+++ index.php
@@ -107,7 +107,7 @@
                     <div class="cof-header">
                         <div class="box-title">
                             <div class="box-title-content">
-                                <p style="font-size: 28px; font-weight: bold;" class="section-title">All About Disert </p>
+                                <p style="font-size: 28px; font-weight: bold;" class="section-title">All About Dessert </p>
                             </div>
                         </div>
                         <div class="row row-cols-1 row-cols-md-4 g-4">
@@ -115,8 +115,8 @@
                                 <div class="card h-100">
                                     <img src="img/disert 1.jpg" class="card-img-top" style="height: 195px;">
                                     <div class="card-body">
-                                        <h5 class="card-title">Ice Blended</h5>
-                                        <p class="card-text">Pop Ice adalah minuman serbuk mengandung Susu yang diproduksi oleh PT. Forisa Nusapersad</p>
+                                        <h5 class="card-title">Cupcake Chocolate</h5>
+                                        <p class="card-text">Tipe dessert kue memiliki ciri khas tekstur yang empuk dan merupakan jenis roti bercita rasa manis.</p>
                                     </div>
                                 </div>
                             </div>
@@ -124,62 +124,62 @@
                                 <div class="card h-100">
                                     <img src="img/disert 2.jpg" class=" card-img-top" style="height: 195px;">
                                     <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                        <h5 class="card-title">Cake Dessert Food</h5>
+                                        <p class="card-text">Cake adalah salah satu jenis makanan yang disajikan sebagai dessert namun harus diperhatikan porsi dan cara penyajiannya. </p>
                                     </div>
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="card h-100">
-                                    <img src="img/disert 3.jpg" class="card-img-top" style="height: 195px;">
+                                    <img src="img/disert3.jpg" class="card-img-top" style="height: 195px;">
                                     <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                        <h5 class="card-title">Baked Egg Custard</h5>
+                                        <p class="card-text">Custard telur panggang pencuci mulut menit terakhir yang ideal,dibuat dengan taburan klasik pala parut, menambahkan sedikit warna dan sedikit bumbu. </p>
                                     </div>
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="card h-100">
-                                    <img src="img/disert 4.jpg" class="card-img-top" style="height: 195px;">
+                                    <img src="img/disert4.jpg" class="card-img-top" style="height: 195px;">
                                     <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                        <h5 class="card-title">Ice Cream Sandwich </h5>
+                                        <p class="card-text">Kue sandwich es krim yang sangat sederhana. Musim panas ditemani Makanan penutup musim panas akan lebih sempurna. </p>
                                     </div>
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="card h-100">
-                                    <img src="img/disert 5.jpg" class="card-img-top" style="height: 195px;">
+                                    <img src="img/disert5.png" class="card-img-top" style="height: 195px;">
                                     <div class="card-body">
-                                        <h5 class="card-title">Ice Blended</h5>
-                                        <p class="card-text">Pop Ice adalah minuman serbuk mengandung Susu yang diproduksi oleh PT. Forisa Nusapersad</p>
+                                        <h5 class="card-title">Cranberry-pear Pie</h5>
+                                        <p class="card-text">Pie adalah salah satu jenis pastry yang berupa adonan biskuit. Dengan tekstur lembut dan rasa yang manis dan lezat</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="card h-100">
-                                    <img src="img/disert 6.jpg" class=" card-img-top" style="height: 195px;">
+                                    <img src="img/disert6.jpg" class=" card-img-top" style="height: 195px;">
                                     <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                        <h5 class="card-title">Permen Caramel</h5>
+                                        <p class="card-text">Cokelat dan permen dibuat melalui tahap kristalisasi gula. Dessert yang masuk ke dalam tipe cokelat dan permen antara lain seperti karamel,lolipop.</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="card h-100">
-                                    <img src="img/disert 7.jpg" class="card-img-top" style="height: 195px;">
+                                    <img src="img//disert 7.jpg" class=" card-img-top" style="height: 195px;">
                                     <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                        <h5 class="card-title">Chocolate Lava Cake</h5>
+                                        <p class="card-text">Kue ini terbilang sangat unik karena dalam kue ini terdapat magma coklat yang sangat lembut dan manis sehingga sangat cocok untuk dicoba. </p>
                                     </div>
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="card h-100">
-                                    <img src="img/disert 8.jpg" class="card-img-top" style="height: 195px;">
+                                    <img src="img/disert8.jpg" class="card-img-top" style="height: 195px;">
                                     <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                        <h5 class="card-title">Vanilla Cream Puff</h5>
+                                        <p class="card-text">Pastry berasal campuran terigu, cairan, dan lemak yang tinggi. Dengan demikian, pastry mempunyai tekstur yang renyah dan berlipat-lipat.</p>
                                     </div>
                                 </div>
                             </div>
