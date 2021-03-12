247c468bbf3d3c68c701622daa33b78f189b4ecc
fadilahnurlaela <fadilahnurlaila366@gmail.com>
2021-03-11 22:08:53 -0800

    menambahkan deskripsi disetiap gambar pada menu minuman

--- index.php
+++ index.php
@@ -11,130 +11,185 @@
 </head>
 
 <body>
-    <!-- <div class="row">
-        <h5 class="card-title"> About Drink</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-
-
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="card" style="width: 18rem;">
-                    <img src="img/drk 1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Ice Blended</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="card" style="width: 18rem;">
-                <img src="img/drk 6.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">coffe hot</p>
-                </div>
-            </div>
-        </div>
-        <div class="card" style="width: 18rem;">
-            <img src="img/drk 7.jpeg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <p class="card-text">Ice green tea</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="card" style="width: 18rem;">
-        <img src="img/drk 2.jpeg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Ice green tea</p>
-        </div>
-    </div>
-    </div>
-    </div> -->
+    <!-- card minuman -->
     <div class="container">
-        <div class="row row-cols-1 row-cols-md-4 g-4">
-            <div class="col">
-                <div class="card h-100">
-                    <img src="img/drk 1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div class="container-custome shop-page best-kopi" id="best-kopi">
+            <div class="cof-header">
+                <div class="box-title">
+                    <div class="box-title-content">
+                        <p style="font-size: 28px; font-weight: bold;" class="section-title"> All About Drink</p>
                     </div>
+                </div>
+                <div class="row row-cols-1 row-cols-md-4 g-4">
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="img/drk 1.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Ice Blended</h5>
+                                <p class="card-text">Pop Ice adalah minuman serbuk mengandung Susu yang diproduksi oleh PT. Forisa Nusapersad</p>
+                            </div>
 
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="img/drk 2.jpeg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                        </div>
                     </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="img/drk 2.jpeg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Ice Kopi Susu</h5>
+                                <p class="card-text">Es kopi susu kekinian dapat dibuat dengan menambahkan gula aren cair. Minuman ini bisa disajikan untuk ngopi pagi sembari sarapan.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="img/drk 3.jpg" class="card-img-top" style="height: 195px;">
+                            <div class="card-body">
+                                <h5 class="card-title">Mojito Deep Blue</h5>
+                                <p class="card-text">Minuman mojito dengan perpaduan rasa blueberry dengan sunquick menjadi rasa minuman lezat</p>
+                            </div>
 
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="img/drk 3.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        </div>
                     </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="img/drk 4.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Bir Pletok Betawi</h5>
+                                <p class="card-text">Minuman ini merupakan minuan penyegar yang dibuat dari campuran rempah-rempah. bir ini minuman yang sangat terkenal di kalangan masyarakat betawi</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row row-cols-1 row-cols-md-4 g-4">
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="img/drk 5.jpg" class="card-img-top" style="height:195px">
+                            <diV class="card-body">
+                                <h5 class="card-title">Brown Sugar Boba Milk</h5>
+                                <p class="card-text">Brown sugar milk tea, minuman teh kekinian yang biasanya dilengkapi dengan topping boba atau pearl ini digandrungi anak-anak hingga dewasa.</p>
+                            </diV>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="img/drk 6.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">coffe hot</h5>
+                                <p class="card-text">Kopi adalah minuman hasil seduhan biji kopi yang telah disangrai dan dihaluskan menjadi bubuk. komoditas yang dibudidayakan lebih dari 50 negara</p>
+                            </div>
 
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="img/drk 4.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
-        <div class="row row-cols-1 row-cols-md-4 g-4">
-            <div class="col">
-                <div class="card h-100">
-                    <img src="img/drk 1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="img/drk 7.jpeg " class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Green Tea Latte</h5>
+                                <p class="card-text">Green teaadalah minuman paling sehat yang kaya akan antioksidan dan nutrisi. Selain itu rasa yang bikin ketagihan.</p>
+                            </div>
 
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="img/drk 2.jpeg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                        </div>
                     </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="img/drk 9.jpg" class="card-img-top" style="height:195px">
+                            <div class="card-body">
+                                <h5 class="card-title">Milkshake Float</h5>
+                                <p class="card-text">Minuman kekinian yang digemari orang karena bukan hanya asik sebagai teman ngobrol, melainkan juga bisa jadi bahan untuk post di Instagram.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <!--akhir drink -->
+                <!-- awal disert -->
+                <div class="container-custome shop-page best-kopi" id="best-kopi">
+                    <div class="cof-header">
+                        <div class="box-title">
+                            <div class="box-title-content">
+                                <p style="font-size: 28px; font-weight: bold;" class="section-title">All About Disert </p>
+                            </div>
+                        </div>
+                        <div class="row row-cols-1 row-cols-md-4 g-4">
+                            <div class="col">
+                                <div class="card h-100">
+                                    <img src="img/disert 1.jpg" class="card-img-top" style="height: 195px;">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Ice Blended</h5>
+                                        <p class="card-text">Pop Ice adalah minuman serbuk mengandung Susu yang diproduksi oleh PT. Forisa Nusapersad</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card h-100">
+                                    <img src="img/disert 2.jpg" class=" card-img-top" style="height: 195px;">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card h-100">
+                                    <img src="img/disert 3.jpg" class="card-img-top" style="height: 195px;">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card h-100">
+                                    <img src="img/disert 4.jpg" class="card-img-top" style="height: 195px;">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card h-100">
+                                    <img src="img/disert 5.jpg" class="card-img-top" style="height: 195px;">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Ice Blended</h5>
+                                        <p class="card-text">Pop Ice adalah minuman serbuk mengandung Susu yang diproduksi oleh PT. Forisa Nusapersad</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card h-100">
+                                    <img src="img/disert 6.jpg" class=" card-img-top" style="height: 195px;">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card h-100">
+                                    <img src="img/disert 7.jpg" class="card-img-top" style="height: 195px;">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card h-100">
+                                    <img src="img/disert 8.jpg" class="card-img-top" style="height: 195px;">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- akhir disert -->
+                        <!-- awal cemilan -->
+                        <!-- akhir cemilan -->
 
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="img/drk 3.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                     </div>
-
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="img/drk 4.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <script src="./js/bootstrap.bundle.min.js"></script>
+                    <script src="./js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
